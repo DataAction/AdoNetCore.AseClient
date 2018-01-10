a937a983f9d99cd366ee1ed69138fb017f5f8aac
Nicholas Sizer <nsizer@da.com.au>
2018-01-10 14:36:45 +1030

    Assert server connection configuration

---
+++ test/AdoNetCore.AseClient.Tests/Integration/HealthCheckTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace AdoNetCore.AseClient.Tests.Integration
+{
+    [SetUpFixture]
+    public class HealthCheckTests
+    {
+        private readonly IDictionary<string, string> _connectionStrings = ConnectionStringLoader.Load();
+
+        [OneTimeSetUp]
+        public void HealthCheckBefore()
+        {
+            using (var connection = new AseConnection(_connectionStrings["default"]))
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandText = "exec sp_configure 'number of user connections'";
+
+                using (var result = command.ExecuteReader())
+                {
+                    result.Read();
+                    var cConfigValue = result.GetOrdinal("Config Value");
+                    var configValue = result.GetValue(cConfigValue).ToString().Trim();
+
+                    Assert.GreaterOrEqual(Convert.ToInt32(configValue), 100, "The database server should be configured to allow for more connections. Run the following: `exec sp_configure 'number of user connections', 100`");
+                }
+            }
+        }
+
+        [OneTimeTearDown]
+        public void HealthCheckAfter()
+        {
+
+        }
+    }
+}

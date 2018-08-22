556993f641a483ef226926637b373de4ef70475f
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-08-22 09:42:39 +0200

    Handle Backup Server messages

--- src/AdoNetCore.AseClient/Internal/Handler/MessageTokenHandler.cs
+++ src/AdoNetCore.AseClient/Internal/Handler/MessageTokenHandler.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Text.RegularExpressions;
 using AdoNetCore.AseClient.Enum;
 using AdoNetCore.AseClient.Interface;
 using AdoNetCore.AseClient.Token;
@@ -11,28 +9,10 @@ namespace AdoNetCore.AseClient.Internal.Handler
     internal class MessageTokenHandler : ITokenHandler
     {
         private readonly List<EedToken> _errorTokens = new List<EedToken>();
+
         public bool CanHandle(TokenType type)
         {
             return type == TokenType.TDS_EED; //add info and error? don't think we'll be messing with capability bits related to that.
-        }
-
-        internal static Tuple<BackupServerErrorMajor, int, BackupServerSeverity, int> parseBackupServerSeverity(string message)
-        {
-            // Backup Server error messages are in this form: MMM DD YYY: Backup Server:N.N.N.N: Message Text
-            // The four components of a Backup Server error message are major.minor.severity.state:
-
-            var regex = new Regex("^Backup Server: ?(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<severity>\\d+)\\.(?<state>\\d+): (?<message>.*)");
-            var groups = regex.Match(message).Groups;
-            var major = int.Parse(groups["major"].Value);
-            var minor = int.Parse(groups["minor"].Value);
-            var severity = int.Parse(groups["severity"].Value);
-            var state = int.Parse(groups["state"].Value);
-
-            return new Tuple<BackupServerErrorMajor, int, BackupServerSeverity, int>(
-                (BackupServerErrorMajor) major, 
-                minor, 
-                (BackupServerSeverity)severity, 
-                state);
         }
 
         public void Handle(IToken token)
@@ -40,11 +20,10 @@ namespace AdoNetCore.AseClient.Internal.Handler
             switch (token)
             {
                 case EedToken t:
-                    var isBackupServer = t.Message.StartsWith("Backup Server:");
-                    
-                    var isSevere = !isBackupServer 
-                        ? t.Severity > 10 
-                        : parseBackupServerSeverity(t.Message).Item3 != BackupServerSeverity.Informational;
+                    var isBackupServer = BackupServerErrorInfo.TryParse(t.Message, out BackupServerErrorInfo backupServerErrorInfo);
+
+                    var isSevere = !isBackupServer ? t.Severity > 10 
+                        : backupServerErrorInfo.Severity != BackupServerSeverity.Informational;
 
                     var msgType = isSevere
                         ? "ERROR"

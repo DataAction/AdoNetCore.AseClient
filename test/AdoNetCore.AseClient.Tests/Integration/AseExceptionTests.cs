a56c07e71e93b04afd11a05d805821ae46d6a1d0
Nicholas Sizer <nsizer@da.com.au>
2018-08-27 11:57:55 +0930

    Add/update tests related to AseException and backup server

---
+++ test/AdoNetCore.AseClient.Tests/Integration/AseExceptionTests.cs
@@ -0,0 +1,38 @@
+using Dapper;
+using NUnit.Framework;
+
+namespace AdoNetCore.AseClient.Tests.Integration
+{
+    public class AseExceptionTests
+    {
+#if NET_FRAMEWORK
+        [Test]
+        public void Execute_InvalidBackupServerCommand_ThrowsAseException_Sap()
+        {
+            using (var connection = new Sybase.Data.AseClient.AseConnection(ConnectionStrings.Pooled))
+            {
+                var dbName = connection.QuerySingle<string>("select db_name()");
+
+                var ex = Assert.Throws<Sybase.Data.AseClient.AseException>(() => connection.Execute($"dump database {dbName} to '/doesnotexist/foo' with compression = '101'"));
+                Assert.Greater(ex.Errors.Count, 1);
+                Assert.Greater(ex.Errors[0].Severity, 10);
+                Assert.AreEqual(8009, ex.Errors[0].MessageNumber);
+            }
+        }
+#endif
+
+        [Test]
+        public void Execute_InvalidBackupServerCommand_ThrowsAseException_CoreFx()
+        {
+            using (var connection = new AseConnection(ConnectionStrings.Pooled))
+            {
+                var dbName = connection.QuerySingle<string>("select db_name()");
+
+                var ex = Assert.Throws<AseException>(() => connection.Execute($"dump database {dbName} to '/doesnotexist/foo' with compression = '101'"));
+                Assert.Greater(ex.Errors.Count, 1);
+                Assert.Greater(ex.Errors[0].Severity, 10);
+                Assert.AreEqual(8009, ex.Errors[0].MessageNumber);
+            }
+        }
+    }
+}

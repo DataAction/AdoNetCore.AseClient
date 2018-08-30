ff4d5cb1e787aad35f6a00ba377b98a3343ea8e4
Nicholas Sizer <nsizer@da.com.au>
2018-08-30 09:31:03 +0930

    Add AseDbType.Image tests

---
+++ test/AdoNetCore.AseClient.Tests/Integration/Query/AseImageTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using AdoNetCore.AseClient.Tests.ConnectionProvider;
+using NUnit.Framework;
+
+namespace AdoNetCore.AseClient.Tests.Integration.Query
+{
+    [Category("basic")]
+#if NET_FRAMEWORK
+    [TestFixture(typeof(SapConnectionProvider))]
+#endif
+    [TestFixture(typeof(CoreFxConnectionProvider))]
+    public class AseImageTests<T> where T : IConnectionProvider
+    {
+        private DbConnection GetConnection()
+        {
+            return Activator.CreateInstance<T>().GetConnection(ConnectionStrings.Pooled);
+        }
+
+        [TestCaseSource(nameof(TestParameter_Cases))]
+        public void SelectParameter_ExecuteScalar(object parameterValue, object expected)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "select @expected";
+                    var p = command.CreateParameter();
+#if NET_FRAMEWORK
+                    if (p is Sybase.Data.AseClient.AseParameter pSap)
+                    {
+                        pSap.AseDbType = Sybase.Data.AseClient.AseDbType.Image;
+                    }
+#endif
+                    if (p is AseParameter pCore)
+                    {
+                        pCore.AseDbType = AseDbType.Image;
+                    }
+
+                    p.ParameterName = "@expected";
+                    p.Value = parameterValue ?? DBNull.Value;
+                    command.Parameters.Add(p);
+                    Assert.AreEqual(expected ?? DBNull.Value, command.ExecuteScalar());
+                }
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TestParameter_Cases()
+        {
+            yield return new TestCaseData(null, null);
+            yield return new TestCaseData(new byte[0], null);
+            yield return new TestCaseData(new byte[] { 0xff }, new byte[] { 0xff });
+        }
+    }
+}

a5285af0e0d354f2ab735f38fd2a01accf37014d
Nicholas Sizer <nsizer@da.com.au>
2018-11-05 09:30:36 +1030

    Add public static void ClearPools() to AseConnection NOTE: ClearPool/ClearPools are not implemented

--- src/AdoNetCore.AseClient/Interface/IConnectionPoolManager.cs
+++ src/AdoNetCore.AseClient/Interface/IConnectionPoolManager.cs
@@ -1,9 +1,10 @@
-﻿namespace AdoNetCore.AseClient.Interface
+namespace AdoNetCore.AseClient.Interface
 {
     internal interface IConnectionPoolManager
     {
         IInternalConnection Reserve(string connectionString, IConnectionParameters parameters);
         void Release(string connectionString, IInternalConnection connection);
         void ClearPool(string connectionString);
+        void ClearPools();
     }
 }

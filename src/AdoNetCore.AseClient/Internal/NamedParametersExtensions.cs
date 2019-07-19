2b0604c1515040e9026682e165a2c73ae2fd96c1
Samuel Kittel <S.S.M.Kittel+github@gmail.com>
2019-07-19 23:12:28 +0930

    Replace parameteriser with single regexp that tokenizes the query string

--- src/AdoNetCore.AseClient/Internal/NamedParametersExtensions.cs
+++ src/AdoNetCore.AseClient/Internal/NamedParametersExtensions.cs
@@ -1,46 +1,84 @@
+using System;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace AdoNetCore.AseClient.Internal
 {
     internal static class NamedParametersExtensions
     {
-        private const string Placeholder = "3f256325-c423-49bb-a3ce-7911c5531d95";
+        // All patterns are intended to be joined into a single regexp pattern
+        // As such, they are all intended to be mutually exclusive
+        // i.e. starting at any index i, no two patterns should find a match starting at the same position j
 
-        private static readonly Regex SingleQuoteRegex = new Regex(
-            $@"(?<='[^']*)({Placeholder})(?=[^']*')",
-            RegexOptions.Compiled | RegexOptions.Multiline);
+        // SQL quoted string 'strings with '' escaping'
+        internal const string AnsiString = "'(?:[^']|'')*'";
 
-        private static readonly Regex SquareBracketRegex = new Regex(
-            $@"(?<=\[[^]]*)({Placeholder})(?=[^]]*\])",
-            RegexOptions.Compiled | RegexOptions.Multiline);
+        // SQL quoted identifier "id with "" escaping"
+        internal const string AnsiQuotedIdentifier = "\"(?:[^\"]|\"\")*\"";
 
-        private static readonly Regex DoubleQuoteRegex = new Regex(
-            $@"(?<=""[^""]*)({Placeholder})(?=[^""]*"")",
-            RegexOptions.Compiled | RegexOptions.Multiline);
+        // SQL square quoted identifier [id with ]] escaping]
+        internal const string SquareQuotedIdentifier = @"\[(?:[^\]]|\]\])*]";
 
-        private static readonly Regex QuestionMarkParameterRegex = new Regex(
-            $"({Placeholder})",
-            RegexOptions.Compiled | RegexOptions.Multiline);
+        // SQL inline comments --
+        internal const string AnsiLineComment = @"--[^\n]*(?:\n|$)";
+
+        // C style comments, both inline // and block /* */ 
+        internal const string CComment = @"/(?:/[^\n]*(?:\n|$)|\*.*?\*/)";
+
+        // Simple sequence of characters that aren't part of comments, strings,
+        // quoted identifiers, or the question marks we are trying to separate
+        // NOTE: negative lookahead used to consume / and - when they aren't forming a comment
+        // They can be removed, but would require merging all the individual patterns and impact testability and maintainability
+        internal const string SimpleSqlPart = "(?:[^'\"[/?-]|/(?![/*])|-(?!-))+";
+
+        // Final two patterns: one to tokenise sql strings,
+        // and the final to merge the tokenised input and collect positional parameters ?
+
+        // A single non-question-mark piece of sql.
+        // It could be any of a quoted identifier, string, comment, or character sequence
+        internal const string SqlPart = AnsiString
+            + "|" + AnsiQuotedIdentifier
+            + "|" + SquareQuotedIdentifier
+            + "|" + AnsiLineComment
+            + "|" + CComment
+            + "|" + SimpleSqlPart;
+
+        // Matches on a single question mark, or a sequence of sql parts (merged into one string)
+        internal static readonly Regex QuestionableSql = new Regex(
+            "(?:" + SqlPart + ")+|\\?", RegexOptions.Compiled | RegexOptions.Singleline
+        );
 
         public static string ToNamedParameters(this string query)
         {
-            // Step 1 - replace all '?' with placeholder.
-            query = query.Replace("?", Placeholder);
+            // Break apart the query, replace all parameters (isolated as single ? strings),
+            // and rejoin them all to reform the string using named parameters
 
-            // Step 2 - replace instances of the placeholder between square brackets with the original question mark.
-            query = SquareBracketRegex.Replace(query, "?");
+            var matches = QuestionableSql.Matches(query);
+            var replacedSql = new StringBuilder();
+            int sqlLength = 0;
+            int paramIndex = 0;
+            foreach (Match m in matches)
+            {
+                string val = m.Value;
+                sqlLength += val.Length;
+                if (val == "?")
+                {
+                    replacedSql.Append("@p").Append(paramIndex);
+                    paramIndex++;
+                }
+                else
+                {
+                    replacedSql.Append(val);
+                }
+            }
 
-            // Step 3 - replace instances of the placeholder between double quotes with the original question mark.
-            query = DoubleQuoteRegex.Replace(query, "?");
-
-            // Step 4 - replace instances of the placeholder between double quotes with the original question mark.
-            query = SingleQuoteRegex.Replace(query, "?");
-
-            // Step 5 - replace instances of the placeholder with parameters.
-            var i = 0;
-            string Evaluator(Match match) => $"@p{i++}";
-            
-            return QuestionMarkParameterRegex.Replace(query, Evaluator);
+            // Verify query was separated correctly
+            // This should only trigger on unbalanced inputs, eg "select 'abc from foo"
+            if (sqlLength != query.Length)
+            {
+                throw new ArgumentException($"Input query did not split cleanly: {query}");
+            }
+            return replacedSql.ToString();
         }
     }
 }
